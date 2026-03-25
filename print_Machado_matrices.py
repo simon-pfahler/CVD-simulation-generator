130eace55ec3e2cc6902ba362c7586806e040341
simon-pfahler <simon-pfahler@gmx.de>
2026-03-25 12:47:49 +0100

    Add Machado matrices for tritanopia

--- print_Machado_matrices.py
+++ print_Machado_matrices.py
@@ -21,7 +21,6 @@ print("}")
 print("Near-null space")
 print(null_space(M, rcond=1e-3))
 
-
 print("Deuteranopia")
 print("{")
 for alpha_M_index in range(11):
@@ -38,7 +37,25 @@ print("}")
 print("Near-null space")
 print(null_space(M, rcond=1e-3))
 
+print("Tritanopia")
+print("{")
+for lambda_S_index in range(11):
+    lambda_S = lambda_S_index * 6
+
+    M = get_Machado_matrix(sensitivities, spds_rgb_lcd, lambda_S=lambda_S)
+
+    print(
+        f"        {{ {{ {M[0,0]:.6f}, {M[0,1]:.6f}, {M[0,2]:.6f} }},"
+        f" {{ {M[1,0]:.6f}, {M[1,1]:.6f}, {M[1,2]:.6f} }},"
+        f" {{ {M[2,0]:.6f}, {M[2,1]:.6f}, {M[2,2]:.6f} }} }},"
+    )
+print("}")
+print("Near-null space")
+print(null_space(M, rcond=1e-3))
+
+
 print("\nCMY conversion:")
+
 print("Protanopia")
 print("{")
 for alpha_L_index in range(11):
@@ -61,6 +78,23 @@ for alpha_M_index in range(11):
     alpha_M = alpha_M_index / 10
 
     M = get_Machado_matrix(sensitivities, spds_cmy_abebe, alpha_M=alpha_M)
+
+    print(
+        f"        {{ {{ {M[0,0]:.6f}, {M[0,1]:.6f}, {M[0,2]:.6f} }},"
+        f" {{ {M[1,0]:.6f}, {M[1,1]:.6f}, {M[1,2]:.6f} }},"
+        f" {{ {M[2,0]:.6f}, {M[2,1]:.6f}, {M[2,2]:.6f} }} }},"
+    )
+print("}")
+print("Near-null space")
+print(null_space(M, rcond=1e-3))
+
+
+print("Tritanopia")
+print("{")
+for lambda_S_index in range(11):
+    lambda_S = lambda_S_index * 6
+
+    M = get_Machado_matrix(sensitivities, spds_cmy_abebe, lambda_S=lambda_S)
 
     print(
         f"        {{ {{ {M[0,0]:.6f}, {M[0,1]:.6f}, {M[0,2]:.6f} }},"
